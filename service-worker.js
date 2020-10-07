10417520bd74b4cf9a93a0b841d9d569dd666de8
Andrey Sergeev <andreyser07@gmail.com>
2020-10-07 18:10:17 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.79d473e8cdf1eed9521b41893f518c07.js"
+  "/precache-manifest.2559f05ce547d92064422dfb8e4c5f5b.js"
 );
 
 self.addEventListener('message', (event) => {
